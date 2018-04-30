e807be7a4bad184f811d4348949991488c3f6615
Zelda000 <38864594+Zelda000@users.noreply.github.com>
2018-04-30 20:22:13 +0200

    Update kod.vhd

--- kod.vhd
+++ kod.vhd
@@ -32,10 +32,10 @@ architecture Behavioral of top is
         signal H: std_logic_vector(5 downto 0) := "001101";
         signal  POM,POM1,POM2,POM3,POM4,POM5,POM6,POM7,POM8,POM9,POM10,
                 POM11,POM12,POM13,POM14,POM15,POM16,POM17,POM18,POM19,POM20,
-				POM21,POM22,POM23,POM24,POM25,POM26,POM27,POM28,POM29,POM30,
-				POM31,POM32,POM33,POM34,POM35,POM36,POM37,POM38,POM39,POM40,
-				POM41,POM42,POM43,POM44,POM45,POM46,POM47,POM48,POM49,POM50,
-				POM51,POM52,POM53,POM54,POM55,POM56,POM57,POM58,POM59 : std_logic_vector(7 downto 0);
+		POM21,POM22,POM23,POM24,POM25,POM26,POM27,POM28,POM29,POM30,
+		POM31,POM32,POM33,POM34,POM35,POM36,POM37,POM38,POM39,POM40,
+		POM41,POM42,POM43,POM44,POM45,POM46,POM47,POM48,POM49,POM50,
+		POM51,POM52,POM53,POM54,POM55,POM56,POM57,POM58,POM59 : std_logic_vector(7 downto 0);
         
 begin
     process (CLK)
@@ -61,32 +61,32 @@ begin
     process (clk_1)
     begin
         if rising_edge(clk_1) then
-                    S <= S + "000001";
-                    if M = "000000" then
+                S <= S + "000001";
+                if M = "000000" then
                         M <= "000001";
-                    end if;
+                end if;
                     
-                    if S = "000000" then
+                if S = "000000" then
                         S <= "000010";
-                    end if;
+                end if;
                     
-                    if H = "000000" then
+                if H = "000000" then
                         H <= "000001";
-                    end if;
+                end if;
             
-					if S = "111100" then
-						S <= "000000";
-						M <= M + "000001";
-					end if;
+		if S = "111100" then
+			S <= "000000";
+			M <= M + "000001";
+		end if;
 			
-					if M = "111101" then
-						H<= H + 1;
+		if M = "111101" then
+			H<= H + 1;
                         M<= "000000";
-					end if;
+		end if;
 			
-					if H = "011001" then
-						H<= "000001";
-					end if;    
+		if H = "011001" then
+			H<= "000001";
+		end if;    
         end if;          
     end process;
 
@@ -104,61 +104,61 @@ begin
                                 POM7 when "001000",
                                 POM8 when "001001",
                                 POM9 when "001010",
-								POM10 when "001011",
-								POM11 when "001100",
-								POM12 when "001101",
-								POM13 when "001110",
-								POM14 when "001111",
-								POM15 when "010000",
-								POM16 when "010001",
-								POM17 when "010010",
-								POM18 when "010011",
-								POM19 when "010100",
-								POM20 when "010101",
-								POM21 when "010110",
-								POM22 when "010111",
-								POM23 when "011000",
-								POM24 when "011001",
-								POM25 when "011010",
-								POM26 when "011011",
-								POM27 when "011100",
-								POM28 when "011101",
-								POM29 when "011110",
-								POM30 when "011111",
-								POM31 when "100000",
-								POM32 when "100001",
-								POM33 when "100010",
-								POM34 when "100011",
-								POM35 when "100100",
-								POM36 when "100101",
-								POM37 when "100110",
-								POM38 when "100111",								
-								POM39 when "101000",
-								POM40 when "101001",
-								POM41 when "101010",
-								POM42 when "101011",
-								POM43 when "101100",
-								POM44 when "101101",
-								POM45 when "101110",
-								POM46 when "101111",
-								POM47 when "110000",
-								POM48 when "110001",
-								POM49 when "110010",
-								POM50 when "110011",
-								POM51 when "110100",
-								POM52 when "110101",
-								POM53 when "110110",
-								POM54 when "110111",
-								POM55 when "111000",
-								POM56 when "111001",
-								POM57 when "111010",
-								POM58 when "111011",
-								POM59 when "111100",
+				POM10 when "001011",
+				POM11 when "001100",
+				POM12 when "001101",
+				POM13 when "001110",
+				POM14 when "001111",
+				POM15 when "010000",
+				POM16 when "010001",
+				POM17 when "010010",
+				POM18 when "010011",
+				POM19 when "010100",
+				POM20 when "010101",
+				POM21 when "010110",
+				POM22 when "010111",
+				POM23 when "011000",
+				POM24 when "011001",
+				POM25 when "011010",
+				POM26 when "011011",
+				POM27 when "011100",
+				POM28 when "011101",
+				POM29 when "011110",
+				POM30 when "011111",
+				POM31 when "100000",
+				POM32 when "100001",
+				POM33 when "100010",
+				POM34 when "100011",
+				POM35 when "100100",
+				POM36 when "100101",
+				POM37 when "100110",
+				POM38 when "100111",								
+				POM39 when "101000",
+				POM40 when "101001",
+				POM41 when "101010",
+				POM42 when "101011",
+				POM43 when "101100",
+				POM44 when "101101",
+				POM45 when "101110",
+				POM46 when "101111",
+				POM47 when "110000",
+				POM48 when "110001",
+				POM49 when "110010",
+				POM50 when "110011",
+				POM51 when "110100",
+				POM52 when "110101",
+				POM53 when "110110",
+				POM54 when "110111",
+				POM55 when "111000",
+				POM56 when "111001",
+				POM57 when "111010",
+				POM58 when "111011",
+				POM59 when "111100",
                                 POM   when others;
                         
         with H select 
                 GR  <=          POM when  "000001", 
-                                POM1 when "000010",
+                                 POM1 when "000010",
                                 POM2 when "000011",   
                                 POM3 when "000100",
                                 POM4 when "000101",
@@ -167,57 +167,57 @@ begin
                                 POM7 when "001000",
                                 POM8 when "001001",
                                 POM9 when "001010",
-								POM10 when "001011",
-								POM11 when "001100",
-								POM12 when "001101",
-								POM13 when "001110",
-								POM14 when "001111",
-								POM15 when "010000",
-								POM16 when "010001",
-								POM17 when "010010",
-								POM18 when "010011",
-								POM19 when "010100",
-								POM20 when "010101",
-								POM21 when "010110",
-								POM22 when "010111",
-								POM23 when "011000",
-								POM24 when "011001",
-								POM25 when "011010",
-								POM26 when "011011",
-								POM27 when "011100",
-								POM28 when "011101",
-								POM29 when "011110",
-								POM30 when "011111",
-								POM31 when "100000",
-								POM32 when "100001",
-								POM33 when "100010",
-								POM34 when "100011",
-								POM35 when "100100",
-								POM36 when "100101",
-								POM37 when "100110",
-								POM38 when "100111",								
-								POM39 when "101000",
-								POM40 when "101001",
-								POM41 when "101010",
-								POM42 when "101011",
-								POM43 when "101100",
-								POM44 when "101101",
-								POM45 when "101110",
-								POM46 when "101111",
-								POM47 when "110000",
-								POM48 when "110001",
-								POM49 when "110010",
-								POM50 when "110011",
-								POM51 when "110100",
-								POM52 when "110101",
-								POM53 when "110110",
-								POM54 when "110111",
-								POM55 when "111000",
-								POM56 when "111001",
-								POM57 when "111010",
-								POM58 when "111011",
-								POM59 when "111100",
-                                POM when others;
+				POM10 when "001011",
+				POM11 when "001100",
+				POM12 when "001101",
+				POM13 when "001110",
+				POM14 when "001111",
+				POM15 when "010000",
+				POM16 when "010001",
+				POM17 when "010010",
+				POM18 when "010011",
+				POM19 when "010100",
+				POM20 when "010101",
+				POM21 when "010110",
+				POM22 when "010111",
+				POM23 when "011000",
+				POM24 when "011001",
+				POM25 when "011010",
+				POM26 when "011011",
+				POM27 when "011100",
+				POM28 when "011101",
+				POM29 when "011110",
+				POM30 when "011111",
+				POM31 when "100000",
+				POM32 when "100001",
+				POM33 when "100010",
+				POM34 when "100011",
+				POM35 when "100100",
+				POM36 when "100101",
+				POM37 when "100110",
+				POM38 when "100111",								
+				POM39 when "101000",
+				POM40 when "101001",
+				POM41 when "101010",
+				POM42 when "101011",
+				POM43 when "101100",
+				POM44 when "101101",
+				POM45 when "101110",
+				POM46 when "101111",
+				POM47 when "110000",
+				POM48 when "110001",
+				POM49 when "110010",
+				POM50 when "110011",
+				POM51 when "110100",
+				POM52 when "110101",
+				POM53 when "110110",
+				POM54 when "110111",
+				POM55 when "111000",
+				POM56 when "111001",
+				POM57 when "111010",
+				POM58 when "111011",
+				POM59 when "111100",
+                                POM   when others;
        
        
        
@@ -234,7 +234,7 @@ begin
                             "01011010" when "101",    
                             "01011010" when "110",
                             "01011010" when "111",
-							"00011000" when "000",
+		            "00011000" when "000",
                             "11111111" when others; 
 
         with a select                         
